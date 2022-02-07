b99bae7e902e5124a4810e13cd15f4b364d5de66
VTHMgNPipola <web.anunciante@gmail.com>
2022-02-07 12:53:07 -0300

    Timer inútil em CommandShooter removido.

--- 2022-RapidReact/src/main/java/frc/robot/commands/CommandShooter.java
+++ 2022-RapidReact/src/main/java/frc/robot/commands/CommandShooter.java
@@ -4,13 +4,10 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Shooter;
 
 public class CommandShooter extends CommandBase {
-    private final Timer timer = new Timer();
     private final Shooter shooter;
 
     public CommandShooter(Shooter shooter) {
@@ -19,16 +16,9 @@ public class CommandShooter extends CommandBase {
     }
 
     @Override
-    public void initialize() {
-        timer.reset();
-        timer.start();
-    }
-
-    @Override
     public void execute() {
         shooter.turnStandShooter();
     }
-
 
     @Override
     public void end(boolean interrupted) {
