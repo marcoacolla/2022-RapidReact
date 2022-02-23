914722b9fc7bc75937645383ed69ed599e844752
Maria Luiza dos Santos <maria.ls2005@aluno.ifsc.edu.br>
2022-02-23 15:48:46 -0300

    Mudanças finais

--- 2022-RapidReact/src/main/java/frc/robot/commands/InvertMotor.java
+++ 2022-RapidReact/src/main/java/frc/robot/commands/InvertMotor.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.subsystems.Intake;
 
 public class InvertMotor extends CommandBase {
@@ -24,7 +25,7 @@ public class InvertMotor extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    r_intake.grabBalls(-0.5);
+    r_intake.grabBalls(-Constants.GB_VELOCITY);
   }
 
   // Called once the command ends or is interrupted.
